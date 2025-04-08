9eba572f8a5825c9ef6d09266f3969056538e776
ramcharan.kasinaboina <ramcharan.kasinaboina@wavemaker.com>
2025-04-08 11:33:18 +0530

    Added new Test File

---
+++ cypress/e2e/FlakyTestsInSelinium.cy.js
@@ -0,0 +1,8 @@
+import LoginPage from "./Pages/LoginPage";
+import ProjectManager from "./Pages/ProjectManager";
+import ProjectWorkspace from "./Pages/ProjectWorkspace";
+import DndWidget from "./Pages/DndWidget";
+
+describe('WidgetPropertiesTest',() =>{
+    
+})
